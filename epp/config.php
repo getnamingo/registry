b6ce7b8e08b72110b7734cd42737d004bed83b64
Pinga <121483313+getpinga@users.noreply.github.com>
2023-08-15 13:21:09 +0300

    EPP configuration fix

--- epp/config.php
+++ epp/config.php
@@ -1,15 +1,16 @@
 <?php
 
 return [
-    'mysql_host' => 'localhost',
-    'mysql_port' => 3306,
-    'mysql_database' => 'your_database_name',
-    'mysql_username' => 'your_username',
-    'mysql_password' => 'your_password',
+    'db_type' => 'mysql',
+    'db_host' => 'localhost',
+    'db_port' => 3306,
+    'db_database' => 'your_database_name',
+    'db_username' => 'your_username',
+    'db_password' => 'your_password',
     'epp_host' => '0.0.0.0',
     'epp_port' => 700,
-	'epp_pid' => '/var/run/epp.pid',
-	'epp_greeting' => 'Namingo EPP Server 1.0',
+    'epp_pid' => '/var/run/epp.pid',
+    'epp_greeting' => 'Namingo EPP Server 1.0',
     'ssl_cert' => '',
     'ssl_key' => '',
 ];
