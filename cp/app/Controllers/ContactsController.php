a5fe1be070e2cbff6ba48bfbd6ebb878722effd7
Pinga <121483313+getpinga@users.noreply.github.com>
2023-09-03 18:02:28 +0300

    UX update for contact create

--- cp/app/Controllers/ContactsController.php
+++ cp/app/Controllers/ContactsController.php
@@ -6,6 +6,7 @@ use App\Models\Contact;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Container\ContainerInterface;
+use League\ISO3166\ISO3166;
 
 class ContactsController extends Controller
 {
@@ -16,7 +17,15 @@ class ContactsController extends Controller
 
     public function create(Request $request, Response $response)
     {
-        return view($response,'admin/contacts/create.twig');
+        $iso3166 = new ISO3166();
+        $db = $this->container->get('db');
+        $countries = $iso3166->all();
+        $registrars = $db->select("SELECT id, clid, name FROM registrar");
+        
+        // Default view for GET requests or if POST data is not set
+        return view($response,'admin/contacts/create.twig', [
+            'registrars' => $registrars,
+            'countries' => $countries,
+        ]);
     }
-
 }
