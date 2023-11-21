7fc56f3866813e298d87795d7542e2a97c12ff64
Pinga <121483313+getpinga@users.noreply.github.com>
2023-11-21 16:40:05 +0200

    Login page fixes and UI cleanup

--- cp/app/Controllers/Auth/AuthController.php
+++ cp/app/Controllers/Auth/AuthController.php
@@ -22,68 +22,6 @@ class AuthController extends Controller
      * @throws \DI\DependencyException
      * @throws \DI\NotFoundException
      */
-    public function createRegister(Request $request, Response $response){
-        return view($response,'auth/register.twig');
-    }
-
-    /**
-     * @param Request $request
-     * @param Response $response
-     * @return Response
-     * @throws \DI\DependencyException
-     * @throws \DI\NotFoundException
-     * @throws \Pinga\Auth\AuthError
-     */
-    public function register(Request $request, Response $response){
-
-        $validation = $this->validator->validate($request, [
-            'email' => v::noWhitespace()->notEmpty()->email(),
-            'username' => v::noWhitespace()->notEmpty()->alnum(),
-            'password' => v::notEmpty()->stringType()->length(8),
-        ]);
-
-        if ($validation->failed()) {
-            redirect()->route('register');
-            //or
-            //return $response->withHeader('Location', route('register'));
-        }
-        $data = $request->getParsedBody();
-        $auth =Auth::create($data['email'],$data['password'],$data['username']);
-        if($auth) {
-            $msg = '<a href="'.route('verify.email.resend',[],['email'=>$data['email']]).'">Resend email</a>';
-            flash('success', 'We have send you a verification link to '.$data['email'].' <br>'.$msg);
-            return $response->withHeader('Location', route('login'));
-        }
-
-    }
-
-    /**
-     * @param Request $request
-     * @param Response $response
-     */
-    public function verifyEmailResend(Request $request, Response $response){
-        $data = $request->getQueryParams();
-        Auth::ResendVerification($data['email']);
-    }
-
-    /**
-     * @param Request $request
-     * @param Response $response
-     * @throws \Pinga\Auth\AuthError
-     */
-    public function verifyEmail(Request $request, Response $response){
-        //confirm email
-        $data = $request->getQueryParams();
-        Auth::verifyEmail($data['selector'], $data['token']);
-    }
-
-    /**
-     * @param Request $request
-     * @param Response $response
-     * @return mixed
-     * @throws \DI\DependencyException
-     * @throws \DI\NotFoundException
-     */
     public function createLogin(Request $request, Response $response){
         return view($response,'auth/login.twig');
     }
--- cp/routes/web.php
+++ cp/routes/web.php
@@ -24,14 +24,8 @@ use Tqdev\PhpCrudApi\Config\Config;
 $app->get('/', HomeController::class .':index')->setName('index');
 
 $app->group('', function ($route) {
-    $route->get('/register', AuthController::class . ':createRegister')->setName('register');
-    $route->post('/register', AuthController::class . ':register');
     $route->get('/login', AuthController::class . ':createLogin')->setName('login');
     $route->post('/login', AuthController::class . ':login');
-
-    $route->get('/verify-email', AuthController::class.':verifyEmail')->setName('verify.email');
-    $route->get('/verify-email-resend',AuthController::class.':verifyEmailResend')->setName('verify.email.resend');
-
     $route->get('/forgot-password', PasswordController::class . ':createForgotPassword')->setName('forgot.password');
     $route->post('/forgot-password', PasswordController::class . ':forgotPassword');
     $route->get('/reset-password', PasswordController::class.':resetPassword')->setName('reset.password');
