aaea2643349f8d9226c573348e342ac2135577bc
Pinga <121483313+getpinga@users.noreply.github.com>
2025-01-21 09:43:02 +0200

    Preparation for v1.0.11

--- docs/install.sh
+++ docs/install.sh
@@ -240,7 +240,7 @@ EOF
     wget "http://www.adminer.org/latest.php" -O /usr/share/adminer/latest.php
     ln -s /usr/share/adminer/latest.php /usr/share/adminer/adminer.php
 
-    git clone --branch v1.0.10 --single-branch https://github.com/getnamingo/registry /opt/registry
+    git clone --branch v1.0.11 --single-branch https://github.com/getnamingo/registry /opt/registry
     mkdir -p /var/log/namingo
     chown -R www-data:www-data /var/log/namingo
     
